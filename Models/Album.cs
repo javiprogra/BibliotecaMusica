7b7c48c2a02eda454b5fe31f11fa63d1db5cbb2a
javiprogra <113139757+javiprogra@users.noreply.github.com>
2025-05-18 21:44:22 -0600

    Implementacion de clsaes y formularios de ingreso de canciones y albumes

---
+++ Models/Album.cs
@@ -0,0 +1,11 @@
+﻿namespace BibliotecaMusica.Models
+{
+    public class Album
+    {
+        public string? tituloAlbum { get; set; }
+        public string? artistaAlbum {  get; set; }
+        public List<Cancion> canciones { get; set; } = new List<Cancion>();
+        public DateTime fechaPublicacion { get; set; }
+
+    }
+}
---
+++ Models/Cancion.cs
@@ -0,0 +1,22 @@
+﻿namespace BibliotecaMusica.Models
+{
+    public class Cancion
+    {
+        public string? nombre { get; set; }
+        public string? artista { get; set; }
+        public string? duracion {  get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Cancion c &&
+                   nombre == c.nombre &&
+                   artista == c.artista &&
+                   duracion == c.duracion;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(nombre, artista, duracion);
+        }
+    }
+}
